1bd5147214d00a338cb456622b2929339f87d423
Rohit Verma <verma.rohit.in@gmail.com>
2022-02-12 17:58:56 +0530

    post comment

--- mediumapp/schemas/schema.js
+++ mediumapp/schemas/schema.js
@@ -9,11 +9,12 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import comment from "./comment";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
@@ -25,5 +26,6 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
+    comment,
   ]),
-})
+});
--- typings.d.ts
+++ typings.d.ts
@@ -1,20 +1,39 @@
+import { Comment } from "./typings.d";
 export interface Post {
-    _id: string;
-    _createdAt: string;
-    title: string;
-    author:{
-        name:string,
-        image:string;
+  _id: string;
+  _createdAt: string;
+  title: string;
+  author: {
+    name: string;
+    image: string;
+  };
+  description: string;
+  mainImage: {
+    asset: {
+      url: string;
     };
-    description:string;mainImage:{
-        asset:{
-            url:string;
-        };
-    };
-    slug:{
-        current:string;
-    };
-    body: [object];
+  };
+  slug: {
+    current: string;
+  };
+  body: [object];
+  comments: Comment[];
+}
+
+export interface Comment {
+  approved: boolean;
+  comment: string;
+  email: string;
+  name: string;
+  post: {
+    _ref: string;
+    _type: string;
+  };
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
 }
 
 // Note: Post[] is same as [Post]
